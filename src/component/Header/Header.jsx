2430a912dfb6d6a53f19a49ae77d2f5262a74d7b
Henaki <mihailboe@mail.ru>
2021-06-07 15:27:07 +0400

    ГОВНО ПОЛНОЕ И Я ТОЖЕ

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,144 +1,173 @@
-import React, { useReducer, useState, useEffect } from 'react';
-import style from './Header.module.css';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useReducer, useState, useEffect } from "react";
+import style from "./Header.module.css";
+import { Link, useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { cyeta } from './../../state'
-import state from '../../state';
-import FormLogin from './FormLogin/FormLogin'
-import FormSignUp from './FormSignUp/FormSignUp'
-import axios from 'axios';
-import { message, Drawer } from 'antd';
-import 'antd/dist/antd.css';
-import Cookies from 'js-cookie';
+import { cyeta } from "./../../state";
+import state from "../../state";
+import FormLogin from "./FormLogin/FormLogin";
+import FormSignUp from "./FormSignUp/FormSignUp";
+import axios from "axios";
+import { message, Drawer } from "antd";
+import "antd/dist/antd.css";
+import Cookies from "js-cookie";
 
 const Header = ({ setUser, user }) => {
+  const [stateSearch, setStateSearch] = useState([]);
+  let [stateSearchTitle, setStateSearchTitle] = useState("");
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
+  let userName = "";
+  let [userNameRole, setUserNameRole] = useState(0);
+  if (userNameRole == 0) {
+    userName = "Гость";
+  } else {
+    state.users.map((user) => {
+      if (user.id == userNameRole) return (userName = user.name);
+    });
+  }
+  let [inputSearch, setInputSearch] = useState("");
+  let [checked_btn, setChecked_btn] = useState(false);
 
-    const [visible, setVisible] = useState(false);
-    const showDrawer = () => {
-        setVisible(true);
-    };
-    const onClose = () => {
-        setVisible(false);
-    };
-    let userName = '';
-    let [userNameRole, setUserNameRole] = useState(0);
-    if (userNameRole == 0) {
-        userName = 'Гость';
-    } else {
-        state.users.map((user) => {
-            if (user.id == userNameRole)
-                return (
-                    userName = user.name
-                )
-        });
+  let history = useHistory();
+
+  window.addEventListener("scroll", () => {
+    const scrolled = window.scrollY;
+    if (scrolled > 100) {
+      setChecked_btn((checked_btn = false));
     }
-    let [state, dispatch] = useReducer(cyeta, "");
-    let textContext = React.createContext(state);
-
-    // let [searchText, setSearchText] = useState('');
-    let [checked_btn, setChecked_btn] = useState(false);
-
-    let history = useHistory();
-
-    window.addEventListener('scroll', () => {
-        const scrolled = window.scrollY;
-        if (scrolled > 100) {
-            setChecked_btn(checked_btn = false);
-        }
-    })
-
-
-    const handleSe = e => {
-        if (true) {
-            history.push("/search")
-            // state.text = searchText
-            // console.log(state.text)            
-        }
-        // else {
-        //     if (searchText.length == 3) {
-        //         alert(`Введите ещё ${4 - searchText.length} символ`)
-        //     } else {
-        //         alert(`Введите ещё ${4 - searchText.length} символа`)
-        //     }
-        // }
-    }
-    return (
-
-        <section>
-            <div className={style.header_main}>
-                <div className={style.header_content}>
-                    <div className="">
-                        <label for="bok__menu" className={style.style_bok_menu}>
-                            <img className={style.style_bok_menu_img} src="icon_filter.png" alt="" width="50px" height="50px" />
-                            <img className={style.style_bok_menu_img_costil} src="icon_filter_costil.png" alt="" width="50px" height="50px" />
-                        </label>
-                        <input type="checkbox" id="bok__menu" className={style.bok_menu_btn} onClick={showDrawer} />
-                        <Drawer
-                            width="300px"
-                            title="Меню"
-                            placement="left"
-                            closable={true}
-                            onClose={onClose}
-                            visible={visible}
-                        >
-                            <ul className={style.list_to_links}>
-                                <div className={style.link_point_menu}>
-                                    <Link to="/" className={style.navlink_menu}>
-                                        <li>Главная</li>
-                                    </Link>
-                                </div>
-                                <div className={style.link_point_menu}>
-                                    <Link className={style.navlink_menu} to="/user">
-                                        <li>Личный кабинет</li>
-                                    </Link>
-                                </div>
-                                <div className={style.link_point_menu}>
-                                    <Link className={style.navlink_menu} to="/shop">
-                                        <li>Каталог</li>
-                                    </Link>
-                                </div>
-                                <div className={style.link_point_menu}>
-                                    <Link className={style.navlink_menu} to="/reviews">
-                                        <li>Отзывы</li>
-                                    </Link>
-                                </div>
-                                <div className={style.link_point_menu}>
-                                    <Link className={style.navlink_menu} to="/pagination">
-                                        <li>FAQ</li>
-                                    </Link>
-                                </div>
-                                <div className={style.link_point_menu}>
-                                    <Link className={style.navlink_menu} to="/pagination">
-                                        <li>Пользовательское соглашение</li>
-                                    </Link>
-                                </div>
-                            </ul>
-                        </Drawer>
-                    </div>
-                    <Link to="/">
-                                <a><img src="logo.png" /></a>
-                            </Link>
-                    <div className={style.search_window_in_header}>
-                        <input type="text" onInput={(e) => dispatch({ type: 'ON_INPUT', payload: e.target.value })} onFocus={(e) => dispatch({ type: 'ON_INPUT', payload: " " })} onBlur={(e) => dispatch({ type: 'ON_INPUT', payload: "" })} className={style.search_input} value={state} name="search" placeholder="search for...
-                        " />
-                        <label onClick={handleSe} className={style.search_button_label} for="search_button" ><img src="loupe.png" className={style.search_button_label_costil} width="30px" height="30px" />
-                            <input type="submit" className={style.search_button} name="search_button" value="" />
-                        </label>
-
-                    </div>
-                    <div className={style.inputs_sign_login}>
-                        <FormSignUp />
-                        <FormLogin />
-                        <div className={style.input_nickname}>
-                            <Link to="/user">
-                                <p className={style.input_nickname__text}>{Cookies.get('login')}</p>
-                            </Link>
-                        </div>
-                    </div>
+  });
+  const handleSe = (e) => {
+    const apiURL = "http://at-shop/api/search/" + inputSearch;
+    axios.get(apiURL).then((data) => {
+        const govno = data.data.data.message
+        let govno3 = data.data.data.content
+        console.log(govno3);
+      Cookies.set("state_costil", govno);
+      Cookies.set("state_costil2", govno3);
+      history.push("/search");
+    });
+  };
+  return (
+    <section>
+      <div className={style.header_main}>
+        <div className={style.header_content}>
+          <div className="">
+            <label for="bok__menu" className={style.style_bok_menu}>
+              <img
+                className={style.style_bok_menu_img}
+                src="icon_filter.png"
+                alt=""
+                width="50px"
+                height="50px"
+              />
+              <img
+                className={style.style_bok_menu_img_costil}
+                src="icon_filter_costil.png"
+                alt=""
+                width="50px"
+                height="50px"
+              />
+            </label>
+            <input
+              type="checkbox"
+              id="bok__menu"
+              className={style.bok_menu_btn}
+              onClick={showDrawer}
+            />
+            <Drawer
+              width="300px"
+              title="Меню"
+              placement="left"
+              closable={true}
+              onClose={onClose}
+              visible={visible}
+            >
+              <ul className={style.list_to_links}>
+                <div className={style.link_point_menu}>
+                  <Link to="/" className={style.navlink_menu}>
+                    <li>Главная</li>
+                  </Link>
                 </div>
+                <div className={style.link_point_menu}>
+                  <Link className={style.navlink_menu} to="/user">
+                    <li>Личный кабинет</li>
+                  </Link>
+                </div>
+                <div className={style.link_point_menu}>
+                  <Link className={style.navlink_menu} to="/shop">
+                    <li>Каталог</li>
+                  </Link>
+                </div>
+                <div className={style.link_point_menu}>
+                  <Link className={style.navlink_menu} to="/reviews">
+                    <li>Отзывы</li>
+                  </Link>
+                </div>
+                <div className={style.link_point_menu}>
+                  <Link className={style.navlink_menu} to="/pagination">
+                    <li>FAQ</li>
+                  </Link>
+                </div>
+                <div className={style.link_point_menu}>
+                  <Link className={style.navlink_menu} to="/pagination">
+                    <li>Пользовательское соглашение</li>
+                  </Link>
+                </div>
+              </ul>
+            </Drawer>
+          </div>
+          <Link to="/">
+            <a>
+              <img src="logo.png" />
+            </a>
+          </Link>
+          <div className={style.search_window_in_header}>
+            <input
+              type="text"
+              className={style.search_input}
+              onInput={(e) => setInputSearch(e.target.value)}
+              name="search"
+              placeholder="search for...
+                        "
+            />
+            <label
+              onClick={handleSe}
+              className={style.search_button_label}
+              for="search_button"
+            >
+              <img
+                src="loupe.png"
+                className={style.search_button_label_costil}
+                width="30px"
+                height="30px"
+              />
+              <input
+                type="submit"
+                className={style.search_button}
+                name="search_button"
+                value=""
+              />
+            </label>
+          </div>
+          <div className={style.inputs_sign_login}>
+            <FormSignUp />
+            <FormLogin />
+            <div className={style.input_nickname}>
+              <Link to="/user">
+                <p className={style.input_nickname__text}>
+                  {Cookies.get("login")}
+                </p>
+              </Link>
             </div>
-        </section>
-    );
-}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Header;
-
--- src/component/SearchInput/SearchInput.jsx
+++ src/component/SearchInput/SearchInput.jsx
@@ -1,17 +1,68 @@
-import style from './SearchInput.module.css';
-import state from './../../state';
-import React, { useState, useEffect, useContext, useReducer } from 'react';
-import {cyeta, text} from '../../state';
-
+import style from "./SearchInput.module.css";
+import React, { useState, useEffect, useContext, useReducer } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
 const SearchInput = () => {
-    // TODO: ЕБАНУТЬ РЕДАКС В ПРОЕКТ
-    let [textSearch, setTextSearch] = useReducer(cyeta, text)
-
-
-    return (
-        <section>
-            <h1>По запросу {textSearch} найдено: {2} товара </h1>
-        </section>
-    );
-}
-export default SearchInput
+  let me_die = "/item/";
+  const [stateSearch, setStateSearch] = useState(Cookies.get('state_costil')); // вернуть все прежде
+  const [qwe, setQwe] = useState([])
+  let stateSearchGovno = Cookies.get("state_costil2");
+  console.log(stateSearchGovno);
+    setQwe(stateSearchGovno)
+  return (
+    <section>
+      <div className={style.main_content_container}>
+        <h1>{Cookies.get('state_costil')} </h1>
+        <div className={style.main_catalog}>
+          {qwe.map((item) => {
+            return (
+              <>
+                <Link
+                  data-aos="fade-up"
+                  to={me_die + item.slag}
+                  className={style.main_catalog__tovar}
+                >
+                  <img src={item.photo} width="200px" height="100px" />
+                  <h3>{item.name}</h3>
+                  <p>{item.price}₽</p>
+                  <input
+                    type="submit"
+                    value="В корзину"
+                    onClick={() => {
+                      const data = {
+                        slag: item.slag,
+                        count: 1,
+                      };
+                      axios
+                        .post("http://at-shop/api/basket", data, {
+                          headers: {
+                            Authorization:
+                              "Bearer " + Cookies.get("jwt_token_debil"),
+                            "X-Requested-With": "XMLHttpRequest",
+                          },
+                        })
+                        .then((data) => {
+                          console.log(data);
+                        })
+                        .catch((error) => {
+                          console.log(error);
+                        });
+                    }}
+                    className={style.button_from_buy}
+                  />
+                  <input
+                    type="submit"
+                    value="Заказать"
+                    className={style.button_from_bought}
+                  />
+                </Link>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+export default SearchInput;
